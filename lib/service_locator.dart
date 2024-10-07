caa1c9413a8c066485ad83b1ec7d7049cd7edc22
nightlan1015297 <dim.1112.work@gmail.com>
2024-10-08 01:08:56 +0800

    chore: Update service_locator.dart with new use cases
    - Add imports for new use cases: GetCourseSchedule, GetCurrentSemester, GetSemesterList
    - Register the new use cases in the service locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,6 +4,9 @@ import 'package:collegenius/data/repositories/auth_repository_impl.dart';
 import 'package:collegenius/data/repositories/course_select_repository_impl.dart'; 
 import 'package:collegenius/domain/repositories/auth_repository.dart';
 import 'package:collegenius/domain/repositories/course_select_repository.dart';
+import 'package:collegenius/domain/usecases/get_course_schedule.dart';
+import 'package:collegenius/domain/usecases/get_current_semester.dart';
+import 'package:collegenius/domain/usecases/get_semester_list.dart';
 import 'package:collegenius/domain/usecases/login_multiple_service.dart';
 import 'package:collegenius/presentation/bloc/auth_bloc.dart';
 import 'package:dio/dio.dart';
@@ -30,7 +33,7 @@ void init() {
   sl.registerLazySingleton<AuthProviderFactory>(() => AuthProviderFactory(dio: sl(), sessionManager: cookieJar));
   
   // Crawler: Register CourseSelectCrawler to handle web scraping for course selection.
-  sl.registerLazySingleton(() => CourseSelectCrawler(dio: sl()));
+  sl.registerLazySingleton(() => CourseSelectCrawler(dio: sl(), sessionManager: cookieJar));
   
   // Repositories: Implement repositories for authentication and course schedule services.
   sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(authProviderFactory: sl()));
@@ -38,6 +41,9 @@ void init() {
   
   // Use Cases: Register the use case for logging into multiple services.
   sl.registerLazySingleton(() => LoginToMultipleServices(sl()));
+  sl.registerLazySingleton(() => GetCourseSchedule(repository:sl()));
+  sl.registerLazySingleton(() => GetCurrentSemester(repository:sl()));
+  sl.registerLazySingleton(() => GetSemesterList(repository:sl()));
   
   // Blocs: Register AuthBloc to manage authentication state in the app.
   sl.registerFactory(() => AuthBloc(loginToMultipleServices: sl()));
