b1f6ef79023933ee9e352fe0b25d029382f91ea1
nightlan1015297 <dim.1112.work@gmail.com>
2024-10-07 10:33:43 +0800

    Add CourseSchedule class for weekly course management
    - Implemented CourseSchedule class to manage daily courses categorized
    by weekdays.
    - Added `isEmpty` getter to check if the schedule has no courses.
    - Introduced `addCourse` method to add a course to a specified weekday,
    validating the existence of the weekday.
    - Provided `getCoursesForDay` method to retrieve daily courses for a
    given weekday.
    - Overrode `toString` for better representation of the CourseSchedule
    instance.

---
+++ lib/domain/entities/course_schedual.dart
@@ -0,0 +1,36 @@
+import 'package:collegenius/data/models/course_schedule_model.dart';
+import 'package:collegenius/data/models/daily_course_model.dart';
+import 'package:collegenius/domain/entities/course.dart';
+import 'package:collegenius/domain/entities/daily_course.dart';
+
+/// Represents the course schedule for a week, categorized by weekdays.
+class CourseSchedule {
+  final Map<WeekDay, DailyCourse> schedule;
+
+  CourseSchedule({required this.schedule});
+
+  /// Checks if the schedule is empty (i.e., all daily courses are empty).
+  bool get isEmpty {
+    return schedule.values.every((dailyCourse) => dailyCourse.isEmpty);
+  }
+
+  /// Adds a course to the specified weekday.
+  void addCourse(WeekDay weekDay, TimeCode timeCode ,Course course) {
+    if (schedule.containsKey(weekDay)) {
+      schedule[weekDay]!.addCourse(code:timeCode,course: course);
+    } else {
+      // Handle the case where the weekday is not found (optional)
+      throw Exception("Invalid weekday: $weekDay");
+    }
+  }
+
+  /// Gets courses for a specific weekday.
+  DailyCourse getCoursesForDay(WeekDay weekDay) {
+    return schedule[weekDay]!; 
+  }
+
+  @override
+  String toString() {
+    return 'CourseSchedule: $schedule';
+  }
+}
