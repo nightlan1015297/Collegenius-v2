2eac399b77538b20ebf2782d2cc015a636e66ddf
nightlan1015297 <dim.1112.work@gmail.com>
2024-10-07 09:13:07 +0800

    Initial commit: Create AuthProviderFactory for handling multiple authentication services
    - Added AuthProviderFactory to manage the instantiation of different
    authentication providers based on the website identifier.
    - The factory currently supports 'CourseSelect' and 'Eeclass' providers,
    returning appropriate instances of CourseSelectAuthProvider and
    EeclassAuthProvider.
    - The factory uses Dio for HTTP requests and CookieJar for session
    management.
    - Throws an UnsupportedError for any unknown or unsupported website
    identifiers.

---
+++ lib/data/data_sources/auth_providers/auth_provider_factory.dart
@@ -0,0 +1,36 @@
+import 'package:collegenius/data/data_sources/auth_providers/auth_provider.dart';
+import 'package:collegenius/data/data_sources/auth_providers/course_select_auth_provider.dart' as course_select;
+import 'package:collegenius/data/data_sources/auth_providers/eeclass_auth_provider.dart' as eeclass;
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:dio/dio.dart';
+
+/// Factory class for creating instances of different authentication providers.
+/// 
+/// The class provides a mechanism to select and instantiate an authentication
+/// provider based on the website identifier (e.g., 'CourseSelect', 'Eeclass').
+/// It relies on [Dio] for handling HTTP requests and [CookieJar] for session 
+/// management across the services.
+class AuthProviderFactory {
+  final Dio dio;
+  final CookieJar sessionManager;
+
+  /// Constructor for initializing the factory with a [Dio] instance 
+  /// for HTTP requests and a [CookieJar] for session management.
+  AuthProviderFactory({required this.dio, required this.sessionManager});
+
+  /// Returns an appropriate [AuthProvider] based on the provided [websiteIdentifier].
+  /// 
+  /// The method uses a switch case to match the identifier with the corresponding 
+  /// authentication provider (e.g., 'CourseSelect' or 'Eeclass'). If the identifier
+  /// doesn't match any known provider, an [UnsupportedError] is thrown.
+  AuthProvider getAuthProvider(String websiteIdentifier) {
+    switch (websiteIdentifier) {
+      case 'CourseSelect':
+        return course_select.CourseSelectAuthProvider(dio: dio, cookieJar: sessionManager);
+      case 'Eeclass':
+        return eeclass.EeclassAuthProvider(dio: dio, cookieJar: sessionManager);
+      default:
+        throw UnsupportedError('Website not supported');
+    }
+  }
+}
