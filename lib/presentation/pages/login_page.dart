3620d7c1875af4d64b6fe402e07cf8fb55ed3988
nightlan1015297 <dim.1112.work@gmail.com>
2024-10-12 18:58:55 +0800

    refactor(login): improve LoginPage with new structure and added result page
    - Refactored `LoginPage` to simplify structure and improve code
    readability.
    - Introduced `LoginResultPage` for displaying authentication results.
    - Replaced `StatefulWidget` with `StatelessWidget` where appropriate.
    - Split `LoginPage` into multiple widgets (`LoginInitPage`,
    `LoginLoadingPage`, etc.) for better maintainability.
    - Removed multi-select feature for websites; simplified login process.
    - Improved error handling and UI feedback for login attempts.

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -1,23 +1,80 @@
 import 'package:collegenius/core/constants.dart';
-import 'package:collegenius/presentation/bloc/auth_bloc.dart';
+import 'package:collegenius/domain/entities/auth_result.dart';
+import 'package:collegenius/presentation/bloc/auth_bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LoginPage extends StatefulWidget {
+/// LoginPage serves as the UI for user authentication.
+/// 
+/// It uses Bloc to manage the state of the login process and handle different
+/// outcomes such as initial loading, login failure, or successful authentication.
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
-  _LoginPageState createState() => _LoginPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state.message),
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthInitial) {
+            return const LoginInitPage();
+          } else if (state is AuthLoading) {
+            return const LoginLoadingPage();
+          } else if (state is AuthenticatedMultiple) {
+            return LoginResultPage(authResults: state.authResults);
+          } else {
+            return const LoginInitPage();
+          }
+        },
+      ),
+    );
+  }
 }
 
-class _LoginPageState extends State<LoginPage> {
+class LoginInitPage extends StatefulWidget {
+  const LoginInitPage({super.key});
+
+  @override
+  LoginInitPageState createState() => LoginInitPageState();
+}
+
+class LoginLoadingPage extends StatelessWidget {
+  const LoginLoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
+
+class LoginFailedPage extends StatelessWidget {
+  const LoginFailedPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Login failed!'),
+    );
+  }
+}
+
+class LoginInitPageState extends State<LoginInitPage> {
   // Controllers for text fields
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
-  // List of available websites (services)
-  final List<WebsiteIdentifier> _availableWebsites = [WebsiteIdentifier.courseSelect, WebsiteIdentifier.eeclass];
-
-  // Selected websites for authentication
-  List<WebsiteIdentifier> _selectedWebsites = [];
 
   @override
   void dispose() {
@@ -30,7 +87,7 @@ class _LoginPageState extends State<LoginPage> {
     final username = _usernameController.text.trim();
     final password = _passwordController.text.trim();
 
-    if (username.isEmpty || password.isEmpty || _selectedWebsites.isEmpty) {
+    if (username.isEmpty || password.isEmpty) {
       // Show error message or handle validation
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Please enter all required fields.')),
@@ -42,95 +99,143 @@ class _LoginPageState extends State<LoginPage> {
     context.read<AuthBloc>().add(LoginButtonPressed(
           username: username,
           password: password,
-          websiteIdentifiers: _selectedWebsites,
+          idents: const [
+            WebsiteIdentifier.courseSelect,
+            WebsiteIdentifier.eeclass
+          ],
         ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: BlocConsumer<AuthBloc, AuthState>(
-        listener: (context, state) {
-          if (state is AuthenticatedMultiple) {
-            // Navigate to the next screen or show success message
-            Navigator.pushReplacementNamed(context, '/home');
-          } else if (state is AuthError) {
-            // Show error message
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text(state.message)),
-            );
-          }
-        },
-        builder: (context, state) {
-          if (state is AuthLoading) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // Username TextField
-                TextField(
-                  controller: _usernameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Username',
-                  ),
-                ),
-                const SizedBox(height: 16),
-                // Password TextField
-                TextField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                  ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 16),
-                // Multi-select for websites
-                _buildWebsiteSelection(),
-                const SizedBox(height: 24),
-                // Login Button
-                ElevatedButton(
-                  onPressed: _onLoginPressed,
-                  child: const Text('Login'),
-                ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Username TextField
+          TextField(
+            controller: _usernameController,
+            decoration: const InputDecoration(
+              labelText: 'Username',
             ),
-          );
-        },
+          ),
+          const SizedBox(height: 16),
+          // Password TextField
+          TextField(
+            controller: _passwordController,
+            decoration: const InputDecoration(
+              labelText: 'Password',
+            ),
+            obscureText: true,
+          ),
+          const SizedBox(height: 24),
+          // Login Button
+          ElevatedButton(
+            onPressed: _onLoginPressed,
+            child: const Text('Login'),
+          ),
+        ],
       ),
     );
   }
+}
 
-  Widget _buildWebsiteSelection() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Select Websites:'),
-        SizedBox(height: 8),
-        Wrap(
-          spacing: 8.0,
-          children: _availableWebsites.map((website) {
-            return FilterChip(
-              label: Text(website.name),
-              selected: _selectedWebsites.contains(website),
-              onSelected: (bool selected) {
-                setState(() {
-                  if (selected) {
-                    _selectedWebsites.add(website);
-                  } else {
-                    _selectedWebsites.remove(website);
-                  }
-                });
-              },
-            );
-          }).toList(),
+class LoginResultPage extends StatelessWidget {
+  final Map<WebsiteIdentifier, AuthResult> authResults;
+  const LoginResultPage({super.key, required this.authResults});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            for (var entry in authResults.entries)
+              Card(
+                margin: const EdgeInsets.symmetric(vertical: 8),
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    minHeight: 90,
+                    minWidth: 340,
+                    maxHeight: 90,
+                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Container(
+                            width: 45,
+                            height: 45,
+                            decoration: BoxDecoration(
+                              color: entry.value.isSuccess
+                                  ? Colors.green
+                                  : Colors.deepOrange,
+                              borderRadius: BorderRadius.circular(22.5),
+                            ),
+                            child: Icon(
+                              entry.value.isSuccess
+                                  ? Icons.verified
+                                  : Icons.error,
+                              size: 30,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 80,
+                        top: 15,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              entry.key.name,
+                              style: theme.textTheme.titleMedium,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 5),
+                            Row(
+                              children: [
+                                OutlinedButton(
+                                  onPressed: entry.value.isSuccess
+                                      ? null
+                                      : () {
+                                          // Re-authentication not yet implemented.
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(const SnackBar(
+                                            content: Text(
+                                                'Re-authentication not implemented yet.'),
+                                          ));
+                                        },
+                                  child: const Text('Re-auth'),
+                                ),
+                                const SizedBox(width: 8),
+                                OutlinedButton(
+                                  onPressed: () {
+                                    // Status check not yet implemented.
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        const SnackBar(
+                                            content: Text(
+                                                'Status check not implemented yet.')));
+                                  },
+                                  child: const Text('Status'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
