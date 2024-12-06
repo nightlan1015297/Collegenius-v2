f9b5f16ea5abe83751a84502a458a8da664f982d
nightlan1015297 <dim.1112.work@gmail.com>
2024-12-06 16:01:42 +0800

    feat: register eeclass usecases and blocs
    Data Sources:
    Registered EeclassCrawler for web scraping functionalities related
    to EEClass.
    
    Repositories:
    Registered EeclassRepositoryImpl as the implementation for
    EeclassRepository, handling data operations for EEClass.
    
    Use Cases:
    Registered various EEClass-related use cases:
    - GetEeclassCourseList: Retrieves a list of EEClass courses.
    - GetEeclassSemesterList: Retrieves a list of EEClass semesters.
    - GetEeclassCourse: Retrieves details for a specific EEClass course.
    - GetEeclassBulletinContent: Retrieves the content of an EEClass
    bulletin.
    - GetEeclassCourseBulletinList: Retrieves a list of bulletins for a
    course.
    - GetEeclassCourseQuizList: Retrieves a list of quizzes for a course.
    
    Blocs:
    Registered Blocs for managing the state of EEClass-related features:
    - EeclassBloc: Manages the state for EEClass overview data.
    - EeclassCourseBloc: Manages the state for individual EEClass courses.
    - EeclassQuizzesBloc: Manages the state for EEClass quizzes.
    - EeclassBulletinsBloc: Manages the state for EEClass bulletins.
    
    Other changes:
    Added Dio client adapter to bypass certificate error during development

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,14 +1,27 @@
 import 'package:collegenius/data/data_sources/auth_providers/auth_provider_factory.dart';
 import 'package:collegenius/data/data_sources/crawlers/course_select_crawler.dart';
+import 'package:collegenius/data/data_sources/crawlers/eeclass_crawler.dart';
 import 'package:collegenius/data/repositories/auth_repository_impl.dart';
 import 'package:collegenius/data/repositories/course_select_repository_impl.dart';
+import 'package:collegenius/data/repositories/eeclass_repository_impl.dart';
 import 'package:collegenius/domain/repositories/auth_repository.dart';
 import 'package:collegenius/domain/repositories/course_select_repository.dart';
+import 'package:collegenius/domain/repositories/eeclass_repository.dart';
 import 'package:collegenius/domain/usecases/get_course_schedule.dart';
 import 'package:collegenius/domain/usecases/get_current_semester.dart';
+import 'package:collegenius/domain/usecases/get_eeclass_bulletin_content.dart';
+import 'package:collegenius/domain/usecases/get_eeclass_course.dart';
+import 'package:collegenius/domain/usecases/get_eeclass_course_bulletin_list.dart';
+import 'package:collegenius/domain/usecases/get_eeclass_course_list.dart';
+import 'package:collegenius/domain/usecases/get_eeclass_course_quiz_list.dart';
+import 'package:collegenius/domain/usecases/get_eeclass_semester_list.dart';
 import 'package:collegenius/domain/usecases/get_semester_list.dart';
 import 'package:collegenius/domain/usecases/login_multiple_service.dart';
 import 'package:collegenius/presentation/bloc/auth_bloc/auth_bloc.dart';
+import 'package:collegenius/presentation/bloc/eeclass/eeclass_bloc.dart';
+import 'package:collegenius/presentation/bloc/eeclass_bulletins/eeclass_bulletins_bloc.dart';
+import 'package:collegenius/presentation/bloc/eeclass_course/eeclass_course_bloc.dart';
+import 'package:collegenius/presentation/bloc/eeclass_quizzes/eeclass_quizzes_bloc.dart';
 import 'package:collegenius/presentation/bloc/schedule/schedule_bloc.dart';
 import 'package:dio/dio.dart';
 // ignore: depend_on_referenced_packages
@@ -21,6 +34,10 @@ final sl = GetIt.instance;
 void init() {
   // Initialize Dio for HTTP requests and CookieJar for session management.
   final dio = Dio();
+  // (dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () =>
+  //     HttpClient()
+  //       ..badCertificateCallback =
+  //           (X509Certificate cert, String host, int port) => true;
   final cookieJar = CookieJar();
 
   // Add CookieManager as an interceptor to Dio for managing cookies.
@@ -37,18 +54,28 @@ void init() {
   // Crawler: Register CourseSelectCrawler to handle web scraping for course selection.
   sl.registerLazySingleton(
       () => CourseSelectCrawler(dio: sl(), sessionManager: cookieJar));
+  sl.registerLazySingleton(() => EeclassCrawler(dio: sl()));
 
   // Repositories: Implement repositories for authentication and course schedule services.
   sl.registerLazySingleton<AuthRepository>(
       () => AuthRepositoryImpl(authProviderFactory: sl()));
   sl.registerLazySingleton<CourseSelectRepository>(() =>
       CourseSelectRepositoryImpl(crsselCrawler: sl(), authRepository: sl()));
+  sl.registerLazySingleton<EeclassRepository>(
+      () => EeclassRepositoryImpl(authRepository: sl(), eeclassCrawler: sl()));
 
   // Use Cases: Register the use case for logging into multiple services.
   sl.registerLazySingleton(() => LoginToMultipleServices(sl()));
   sl.registerLazySingleton(() => GetCourseSchedule(repository: sl()));
   sl.registerLazySingleton(() => GetCurrentSemester(repository: sl()));
   sl.registerLazySingleton(() => GetSemesterList(repository: sl()));
+
+  sl.registerLazySingleton(() => GetEeclassCourseList(repository: sl()));
+  sl.registerLazySingleton(() => GetEeclassSemesterList(repository: sl()));
+  sl.registerLazySingleton(() => GetEeclassCourse(repository: sl()));
+  sl.registerLazySingleton(() => GetEeclassBulletinContent(repository: sl()));
+  sl.registerLazySingleton(() => GetEeclassCourseBulletinList(repository: sl()));
+  sl.registerLazySingleton(() => GetEeclassCourseQuizList(repository: sl()));
 
   // Blocs: Register AuthBloc to manage authentication state in the app.
   sl.registerFactory(() => AuthBloc(loginToMultipleServices: sl()));
@@ -57,5 +84,28 @@ void init() {
         getCourseSchedule: sl(),
         getCurrentSemester: sl(),
         getSemesterList: sl()),
+  );
+  sl.registerFactory(
+    () => EeclassBloc(
+      getEeclassCourseList: sl(),
+      getEeclassSemesterList: sl(),
+    ),
+  );
+  sl.registerFactory(
+    () => EeclassCourseBloc(
+      getEeclassBulletin: sl(),
+      getEeclassBulletinList: sl(),
+      getEeclassCourse: sl(),
+    ),
+  );
+  sl.registerFactory(
+    () => EeclassQuizzesBloc(
+      getEeclassCourseQuizList: sl(),
+    ),
+  );
+   sl.registerFactory(
+    () => EeclassBulletinsBloc(
+      getEeclassBulletinList: sl(),
+    ),
   );
 }
