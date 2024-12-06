8674a4f7da01da99a221118b00dc82b9a67e69a4
nightlan1015297 <dim.1112.work@gmail.com>
2024-12-06 15:57:46 +0800

    feat: convert EeclassSemester to equatable and make constructor const
    - The class EeclassSemester has been updated to extend Equatable. This
    change allows for value-based equality comparisons, meaning two
    instances will be considered equal if their properties are the same.
    - Added override getter props to define the properties that will be used
    for equality comparisons which are id and semesterName.
    - The constructor of EeclassSemester is modified to be a const
    constructor.
    - Update the empty constructor of class EeclassSemester with a const
    keyword.

--- lib/domain/entities/eeclass_semester.dart
+++ lib/domain/entities/eeclass_semester.dart
@@ -1,19 +1,24 @@
 
+import 'package:equatable/equatable.dart';
+
 ///  class representing a semester.
 /// 
 /// This class encapsulates the details of a semester, including its 
 /// year and name. It provides methods to create a Semester from 
 /// a string representation and convert it to a corresponding Semester entity.
-class EeclassSemester {
+class EeclassSemester extends Equatable {
   final String id; // The year of the semester.
   final String semesterName; // The name of the semester (e.g., "1101, 1102...etc").
 
   /// Constructor for initializing Semester with a year and name.
-  EeclassSemester({required this.id, required this.semesterName});
+  const EeclassSemester({required this.id, required this.semesterName});
 
   bool get isEmpty => id.isEmpty && semesterName.isEmpty;
 
   factory EeclassSemester.empty() {
-    return EeclassSemester(id: '', semesterName: '');
+    return const EeclassSemester(id: '', semesterName: '');
   }
+
+  @override
+  List<Object?> get props => [id, semesterName];
 }
